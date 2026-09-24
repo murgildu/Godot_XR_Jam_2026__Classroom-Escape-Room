69788e3495c569d7a5a394fe37a93094474fe430
Inigo Lopez-Gazpio <inigo.lopez@ehu.eus>
2026-09-25 00:05:07 +0200

    add colors according to machine when inserting usb

--- Scripts/calculate_phi_machine_confirmation.gd
+++ Scripts/calculate_phi_machine_confirmation.gd
@@ -2,12 +2,16 @@ extends Node3D
 var isDone: bool = false;
 @onready var FacMachine = get_node("/root/MainStaging/Scene/TestScene/Node3D3")
 @onready var  isDoneIndicator= get_node("/root/MainStaging/Scene/TestScene/StaticBody3D4")
+@onready var room = get_node("../Room")
+@onready var table_material: BaseMaterial3D = get_node("../Floor/MeshInstance3D/Node3D/StaticBody3D/MeshInstance3D").get_active_material(0)
 func _ready() -> void:
 	$SnapTree/SnapZone.has_picked_up.connect(_on_snap_tree_picked_up)
 	$SnapTree/SnapZone.has_dropped.connect(_on_snap_tree_dropped)
 
 func _on_snap_tree_picked_up(what_):
 	if  FacMachine.isDone:
+		if not isDone:
+			room.set_lamp_color(table_material.albedo_color)
 		isDone = true
 		isDoneIndicator.visible=true
 		$SnapTree/instruction_panel2/instruction_panel/instruction_faceOn.visible = true
--- assets/kenney.nl/room.gd
+++ assets/kenney.nl/room.gd
@@ -1,4 +1,34 @@
 extends Node3D
 
-func _ready():
+const COLOR_FADE_SECONDS := 0.7
+
+var color_tween: Tween
+var bulb_materials: Array[BaseMaterial3D] = []
+
+@onready var lamps: Array[OmniLight3D] = [
+	$Ceiling/OmniLight01,
+	$Ceiling/OmniLight02,
+	$Ceiling/OmniLight03,
+]
+
+
+func _ready() -> void:
 	$Ceiling.visible = true
+	for lamp in lamps:
+		var bulb: MeshInstance3D = lamp.get_node("LampMesh")
+		# Keep runtime bulb colors local to this room, including after a restart.
+		var material: BaseMaterial3D = bulb.get_active_material(0).duplicate()
+		bulb.material_override = material
+		bulb_materials.append(material)
+
+
+func set_lamp_color(color: Color) -> void:
+	# A new completion can arrive before the previous fade has finished.
+	if color_tween:
+		color_tween.kill()
+	color_tween = create_tween().set_parallel(true)
+	color_tween.set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
+	for lamp in lamps:
+		color_tween.tween_property(lamp, "light_color", color, COLOR_FADE_SECONDS)
+	for material in bulb_materials:
+		color_tween.tween_property(material, "albedo_color", color, COLOR_FADE_SECONDS)
